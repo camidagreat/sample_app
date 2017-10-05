4f8139dbf97058420b5d7405ec9e571e8c316a94
Luke BC <lbidigarecurtis@alumni.berklee.edu>
2017-10-05 09:21:39 -0400

    wip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,10 +55,10 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
-  
+
   config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
-    host = '<your heroku app>.herokuapp.com'
+    host = 'sample_app.herokuapp.com'
     config.action_mailer.default_url_options = { host: host }
     ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
